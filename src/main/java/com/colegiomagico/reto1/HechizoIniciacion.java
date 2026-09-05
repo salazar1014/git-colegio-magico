124b56b74a0337a1541ff27aa650f71dadc79f78
salazar1014 <emsalazar755@gmail.com>
2026-09-05 15:54:17 +0000

    feat:Se agrega la implementacion de la funcion invocar

--- src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
+++ src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto1;
 public class HechizoIniciacion {
 
     public static String invocar() {
-        return ""; // TODO: Cambia esto
+        return "Mi nombre es Emanuel y el nombre del profe es Juan Jose"; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
