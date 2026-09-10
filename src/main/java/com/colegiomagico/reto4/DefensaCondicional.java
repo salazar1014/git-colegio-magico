42af82dfc135bd2c8838b1d490267d08608af2a2
salazar1014 <emsalazar755@gmail.com>
2026-09-10 22:33:43 +0000

    feat: se agrega el if a la defensacondicional, en el cual si el hechizo es imperius o crucio se lanza la maldicion, de lo contrario es permitido

--- src/main/java/com/colegiomagico/reto4/DefensaCondicional.java
+++ src/main/java/com/colegiomagico/reto4/DefensaCondicional.java
@@ -3,12 +3,15 @@ package com.colegiomagico.reto4;
 /**
  * Si el hechizo es 'Imperius' o 'Crucio', retorna 'Maldicion'. De lo contrario, 'Permitido'.
  */
+
 public class DefensaCondicional {
-
     public static String evaluar(String hechizo) {
-        return ""; // TODO: Cambia esto
+        if (hechizo.equals("Imperius") || hechizo.equals("Crucio")) {
+            return "Maldicion";
+        } else {
+            return "Permitido";
+        }
     }
-
     public static void main(String[] args) {
         System.out.println("Lumos: " + evaluar("Lumos"));
         System.out.println("Crucio: " + evaluar("Crucio"));
