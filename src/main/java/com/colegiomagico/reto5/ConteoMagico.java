f25ce7675002024cb7bfba08ff5db56db1f697ba
salazar1014 <emsalazar755@gmail.com>
2026-09-10 23:32:44 +0000

    feat: Se realizo el while que permite sumar todos los numeros hasta el numero 4, el cual es el limite, como resultado debe retornar la suma de 1+2+3+4

--- src/main/java/com/colegiomagico/reto5/ConteoMagico.java
+++ src/main/java/com/colegiomagico/reto5/ConteoMagico.java
@@ -6,7 +6,15 @@ package com.colegiomagico.reto5;
 public class ConteoMagico {
 
     public static int contar(int limite) {
-        return 0; // TODO: Cambia esto
+        int suma = 0;
+        int i = 1;
+
+        // AQUÍ EMPIEZA EL WHILE:
+        while (i <= limite) {
+            suma += i;
+            i++; 
+        }
+        return suma;
     }
 
     public static void main(String[] args) {
