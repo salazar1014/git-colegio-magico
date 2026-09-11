cb32cf383260cf0f9462e6b4d21606f2a283a678
salazar1014 <emsalazar755@gmail.com>
2026-09-11 00:21:32 +0000

    feat: Se hace un for con un if para poder retornar el numero de mayor tamaño que este puesto dentro del arreglo

--- src/main/java/com/colegiomagico/reto7/CofreVaritas.java
+++ src/main/java/com/colegiomagico/reto7/CofreVaritas.java
@@ -6,7 +6,14 @@ package com.colegiomagico.reto7;
 public class CofreVaritas {
 
     public static int varitaMasPoderosa(int[] tamanos) {
-        return 0; // TODO: Cambia esto
+        int mayor = tamanos[0];
+
+        for (int tamano : tamanos) {
+            if (tamano > mayor) {
+                mayor = tamano;
+            }
+        }
+        return mayor;
     }
 
     public static void main(String[] args) {
