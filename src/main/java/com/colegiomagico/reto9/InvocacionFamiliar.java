feb035c92ec1d5a02fcf46c742e6758e06ef0793
salazar1014 <emsalazar755@gmail.com>
2026-09-11 01:12:01 +0000

    feat:Se hace una funcion en la cual al usarlo se retorna los nombres y el tipo

--- src/main/java/com/colegiomagico/reto9/InvocacionFamiliar.java
+++ src/main/java/com/colegiomagico/reto9/InvocacionFamiliar.java
@@ -6,10 +6,10 @@ package com.colegiomagico.reto9;
 public class InvocacionFamiliar {
 
     public static String invocar(String nombre, String tipo) {
-        return "Familiar: " + tipo + "llamado" + nombre;
+        return "Familiar: " + tipo + " llamado " + nombre;
     }
 
     public static void main(String[] args) {
-        System.out.println(invocar("Nano", "Cabra"));
+        System.out.println(invocar("Hedwig", "Lechuza"));
     }
 }
